deed8fa245383df7780567070a849d6b58cdb39c
cooper <qyp2009.vip@gmail.com>
2015-09-27 22:28:27 +0800

    modify MainActivity.java

--- src/com/example/test123/MainActivity.java
+++ src/com/example/test123/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(APP_OPS_SERVICE, "aaa");
+        Log.d(APP_OPS_SERVICE, "aaabbb");
     }
 
     @Override
